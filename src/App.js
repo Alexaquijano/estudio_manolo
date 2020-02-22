181b058326b11d4989eea568e76b169e96338606
Alexandra Q <aquisa22@hotmail.com>
2020-02-21 21:14:17 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ejemploSelect from "./ejemploSelect";
+
+function App() {
+  return (
+    <div className="App">
+     <ejemploSelect></ejemploSelect>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ejemploSelect.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const ejemploSelect = () => {
+    return(
+        <h1>HOLA</h1>
+//         <select>
+//        <option></option>
+//        <option>Si</option>
+//        <option>No</option>
+//        <option>Tal vez</option>
+//        <option>No aplica</option>
+//    </select>
+    )
+   
+
+}
+
+export default ejemploSelect;
